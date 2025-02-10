1365da326336a7126faf006ea5f135d61c732b75
Lúcia Helena Proença <131099300+Lucia-Helena@users.noreply.github.com>
2025-02-10 00:43:18 -0300

    Projeto finalizado

--- script/script.js
+++ script/script.js
@@ -1,3 +1,19 @@
-function abrirProjeto(url) {
-    window.open(url, '_blank');
-}
+debugger;  
+const projetos = [  
+  { nome: 'Efood - Restaurante delivery', url: 'https://efood-gamma-wheat.vercel.app/' },  
+  { nome: 'Projeto 2', url: 'https://efood-gamma-wheat.vercel.app/' },  
+  { nome: 'Projeto 3', url: 'https://efood-gamma-wheat.vercel.app/' },  
+];  
+
+function handleProjectClick(url) {  
+  window.location.href = url;  
+}  
+
+const projetosContainer = document.querySelectorAll('.projeto');  
+projetosContainer.forEach((container, index) => {  
+  const div = document.createElement('div');  
+  div.textContent = projetos[index].nome;  
+  div.classList.add('projeto-item');  
+  div.addEventListener('click', () => handleProjectClick(projetos[index].url));  
+  container.appendChild(div);  
+});
